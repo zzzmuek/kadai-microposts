b7261ac4a7c75178f696226da4675c53e418360f
test1234 <zzzmuek@mineo.jp>
2019-09-11 05:53:14 +0000

    post

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
-	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
-	end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
-	def logged_in?
-		!!current_user
-	end
+  def logged_in?
+    !!current_user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_064720) do
+ActiveRecord::Schema.define(version: 2019_09_11_010550) do
+
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2019_09_09_064720) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "microposts", "users"
 end
